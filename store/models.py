e47cf249545916f1863f52a5f710902b7962ca5c
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-10-11 13:04:39 +0545

    new

--- store/models.py
+++ store/models.py
@@ -6,13 +6,13 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
-    featured_product = models.ForeignKey('Product',on_delete=models.SET_NULL,null=True)
+    featured_product = models.ForeignKey('Product',on_delete=models.SET_NULL,null=True, related_name='+')
 
 class Product(models.Model):
     sku = models.CharField(primary_key=True)
     title = models.CharField (max_length=300)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2) 
     inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection,on_delete=models.PROTECT)
@@ -64,4 +64,4 @@ class Cart(models.Model):
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
-    quantity = models.PositiveBigIntegerField()
+    quantity = models.PositiveIntegerField()
