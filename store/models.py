15fb2ce72aef8ecaefe86e850898b7e78419a986
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-11-04 15:25:44 +0545

    new

--- store/models.py
+++ store/models.py
@@ -13,7 +13,7 @@ class Product(models.Model):
     title = models.CharField (max_length=300)
     slug = models.SlugField(default='-')
     description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2) 
+    unit_price = models.CharField(max_length=300) 
     inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection,on_delete=models.PROTECT)
@@ -52,7 +52,7 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product  = models.ForeignKey(Product,on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
-    unit_price = models.DecimalField(max_digits=6,decimal_places=2)
+    unit_price = models.CharField(max_length=300) 
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
