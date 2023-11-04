71ff5e841f6b56434434e3179bdbce1b052c8dc1
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-11-04 16:34:55 +0545

    new

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from store.models import Product
 # Create your views here.
 # request -> response
 # request handler
 
 def say_hello(request):
+    quey = Product.objects.filter(last_updated__gt=2021)
     return HttpResponse('hello World')
