701e104b629b406a7ca29e83e87ccbf5eeaeffd3
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-12-16 22:31:37 +0545

    create the add cart class and save the item and increase the quality of item

--- store/serializers.py
+++ store/serializers.py
@@ -36,7 +36,7 @@ class SimpleProductSerializer(serializers.ModelSerializer):
 
 
 class CartItemSerializer(serializers.ModelSerializer):
-    product = SimpleProductSerializer(read_only = True)
+    product = SimpleProductSerializer()
     total_price = serializers.SerializerMethodField()
 
     def get_total_price(self, cart_item:CartItem):
@@ -58,3 +58,21 @@ class CartSerializer(serializers.ModelSerializer):
         model = Cart
         fields = ['id','items','total_price'] 
 
+class ADDCartItemSerializer(serializers.ModelSerializer):
+    product_id = serializers.IntegerField()
+
+    def save(self, **kwargs):
+        cart_id = self.context['cart_id']
+        product_id = self.validated_data['product_id']
+        quantity  = self.validated_data['quantity']
+        try:
+            cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product_id)
+            cart_item.quantity+=quantity
+            cart_item.save()
+            self.instance = cart_item
+        except CartItem.DoesNotExist:
+            self.instance = CartItem.objects.create(cart_id=cart_id , **self.validated_data)
+        return self.instance
+    class Meta:
+        model =  CartItem
+        fields = ['id','product_id','quantity']
